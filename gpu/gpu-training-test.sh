6da70370a526ed17259542437a9d331ef5465df8
Samuel Andersen <samuel@andersentech.net>
2025-06-10 19:48:45 +0000

    Update Fuji test

--- gpu/gpu-training-test.sh
+++ gpu/gpu-training-test.sh
@@ -8,6 +8,9 @@ cd /root/axlearn
 # Install the requirements for AXLearn
 uv pip install .[core,gpu,gcp] 
 
+# Ensure we have no checkpoints stored
+gsutil -m rm -rf gs://axlearn-arc-testing/a4-sa-test/launch_trainer_flags gs://axlearn-arc-testing/a4-sa-test/model_analysis.txt gs://axlearn-arc-testing/a4-sa-test/trainer_config gs://axlearn-arc-testing/a4-sa-test/trainer_state_tree.txt gs://axlearn-arc-testing/a4-sa-test/checkpoints
+
 # Execute the training test job and timeout after 5 minutes
 timeout 300s python3 -m axlearn.common.launch_trainer_main \
     --module=text.gpt.c4_trainer --config=fuji-7B-v2-flash-single-host \
