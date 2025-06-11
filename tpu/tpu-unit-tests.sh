7d7c07fe2febbd2ba707c20e6ccc0316ecaf351a
Samuel Andersen <samuel@andersentech.net>
2025-06-11 23:13:27 +0000

    Update ulimit

--- tpu/tpu-unit-tests.sh
+++ tpu/tpu-unit-tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set ulimit before running tests
+echo "Setting ulimit before tests"
+ulimit -n 1000000
+
 cd /root/axlearn
 
 pytest -v --junit-xml=/home/runner/_work/xml_results/tpu_tests.xml --alluredir /home/runner/_work/allure_results axlearn/common/flash_attention/tpu_attention_test.py || touch /home/runner/_work/test_failed
