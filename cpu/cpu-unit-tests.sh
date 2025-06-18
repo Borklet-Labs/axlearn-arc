d4058195515b62348004049c7afdc6f39cb35388
Samuel Andersen <samuel@andersentech.net>
2025-06-18 21:02:04 +0000

    Add git setup script

--- cpu/cpu-unit-tests.sh
+++ cpu/cpu-unit-tests.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-# Check for custom origin
-if [ -z "$CUSTOM_GIT_ORIGIN" ]; then
-    GIT_ORIGIN="https://github.com/andersensam/axlearn"
-else
-    GIT_ORIGIN="$CUSTOM_GIT_ORIGIN"
-fi
-
-# Check for branch name
-if [ -z "$CUSTOM_GIT_BRANCH" ]; then
-    GIT_BRANCH="main"
-else
-    GIT_BRANCH="$CUSTOM_GIT_BRANCH"
-fi
-
-echo "About to pull branch $GIT_BRANCH from origin $GIT_ORIGIN"
-
-# Grab the latest AXLearn from upstream
-git init /root && cd /root 
-git remote add origin $GIT_ORIGIN
-git -c protocol.version=2 fetch --no-tags --prune --no-recurse-submodules --depth=1 origin
-git checkout $GIT_BRANCH
-
-# Show the commit information
-git log -1 --stat
-
-# Install the GCP utils and PyTest suite
-uv pip install .[core,gpu,gcp,tpu] pytest pytest-instafail allure-pytest torch pytest-xdist
-
-# Clean any previous test results
-rm -rf /home/runner/_work/xml_results
-rm -rf /home/runner/_work/allure_results
-rm pytest_xml.tar.gz allure_results.tar.gz
-
-# Create a test output directory
-mkdir -p /home/runner/_work/xml_results
-mkdir -p /home/runner/_work/allure_results
-
 pytest -v --junit-xml=/home/runner/_work/xml_results/cpu_tests.xml --alluredir /home/runner/_work/allure_results -n 8 $(find axlearn -type f -name "*test*.py" ! -name "*gpu*" ! -name "*vertex*" ! -name "*tpu*" -printf '%p ') || touch /home/runner/_work/test_failed
 
 # Compress the results
