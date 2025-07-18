0d176b1c1bdf81bd51ab8f68228ea07429a3125f
Samuel Andersen <samuel@andersentech.net>
2025-07-18 00:44:28 +0000

    remove junit

--- cpu/cpu-unit-tests.sh
+++ cpu/cpu-unit-tests.sh
@@ -9,18 +9,18 @@ curl https://huggingface.co/t5-base/resolve/main/spiece.model -o axlearn/data/to
 curl https://huggingface.co/FacebookAI/roberta-base/raw/main/merges.txt -o axlearn/data/tokenizers/bpe/roberta-base-merges.txt
 curl https://huggingface.co/FacebookAI/roberta-base/raw/main/vocab.json -o axlearn/data/tokenizers/bpe/roberta-base-vocab.json
 
-# Create five groups of pytest files, needed to avoid OOM with stack trace failures
-groups=(00 01 02 03 04)
+# Create eight groups of pytest files, needed to avoid OOM with stack trace failures
+groups=(00 01 02 03 04 05 06 07)
 find axlearn -type f -name "*_test*.py" ! -name "*gpu*" ! -name "*vertex*" ! -name "*tpu*" > pytest_files.txt
-split -n r/5 -a 2 -d pytest_files.txt split_pytest_files_
+split -n r/8 -a 2 -d pytest_files.txt split_pytest_files_
 touch /home/runner/_work/csv_results/cpu_tests_all_results.csv
 
 for i in ${groups[@]}; do
     echo Starting standard tests for group ${i}
-    pytest -v --junit-xml=/home/runner/_work/xml_results/cpu_tests_${i}.xml \
+    pytest -v  \
         --csv /home/runner/_work/csv_results/cpu_tests_${i}.csv \
         -n auto -m "not (gs_login or tpu or high_cpu or fp64 or for_8_devices)" --durations=100 \
-        $(tr '\n' ' ' < split_pytest_files_${i}) --dist worksteal
+        --tb=native $(tr '\n' ' ' < split_pytest_files_${i}) --dist worksteal
 
     echo Adding results from group ${i} to master CSV
     if [[ "$i" == 00 ]]; then
@@ -44,7 +44,7 @@ done
 
 for i in ${groups[@]}; do
     echo Starting FP64 for group ${i}
-    JAX_ENABLE_X64=1 pytest -v --junit-xml=/home/runner/_work/xml_results/cpu_tests_fp64_${i}.xml \
+    JAX_ENABLE_X64=1 pytest -v \
         --csv /home/runner/_work/csv_results/cpu_tests_fp64_${i}.csv \
         -n auto -m "fp64" --durations=100 \
         $(tr '\n' ' ' < split_pytest_files_${i}) --dist worksteal
@@ -68,7 +68,7 @@ done
 for i in ${groups[@]}; do
     echo Starting for_8_devices tests for group ${i}
     XLA_FLAGS="--xla_force_host_platform_device_count=8" \
-    pytest -v --junit-xml=/home/runner/_work/xml_results/cpu_tests_for_8_devices_${i}.xml \
+    pytest -v \
         --csv /home/runner/_work/csv_results/cpu_tests_for_8_devices_${i}.csv \
         -n auto -m "for_8_devices" --durations=100 \
         $(tr '\n' ' ' < split_pytest_files_${i}) --dist worksteal 
@@ -91,7 +91,7 @@ done
 
 # Compress the results
 cd /home/runner/_work
-tar -czvf results.tar.gz xml_results csv_results
+tar -czvf results.tar.gz csv_results
 timestamp=$(date +"%Y-%m-%d-%T")
 
 # Upload to GCS, including the date and hostname inside the pod
