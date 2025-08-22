3c2e6744ffedf23278c0a3695b258ea5e8158a25
Judy Wu <judyzhaoxinwu@google.com>
2025-08-22 01:08:40 +0000

    Add the JAX nightly

--- tpu/tpu-jobset-startup.sh
+++ tpu/tpu-jobset-startup.sh
@@ -26,6 +26,9 @@ git checkout $GIT_BRANCH
 git log -1 --stat --pretty=format:"%H" --no-patch
 
 uv pip install .[core,tpu]
+pip install -U --pre jax jaxlib libtpu requests -i https://us-python.pkg.dev/ml-oss-artifacts-published/jax/simple/ -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
+echo "Installed JAX nightly"
 
 # Modify the batch size to account for TPU v6e 4x4
 sed -i 's/train_batch_size=train_batch_size/train_batch_size=32/g' /root/axlearn/experiments/text/gpt/fuji.py
