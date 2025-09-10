dd63585a4c84996df61d60367e64f3b5db2191e2
Judy Wu <judyzhaoxinwu@google.com>
2025-09-10 22:29:30 +0000

    Add a sequence action to trigger training tests daily for JAX nightly

--- tpu/tpu-jobset-startup.sh
+++ tpu/tpu-jobset-startup.sh
@@ -27,7 +27,9 @@ git log -1 --stat --pretty=format:"%H" --no-patch
 
 export UV_FIND_LINKS="https://storage.googleapis.com/jax-releases/libtpu_releases.html,https://storage.googleapis.com/axlearn-wheels/wheels.html"
 uv pip install --prerelease=allow .[core,tpu]
+pip install -U --pre jax jaxlib libtpu requests -i https://us-python.pkg.dev/ml-oss-artifacts-published/jax/simple/ -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
+echo "Installed JAX nightly"
 # Modify the batch size to account for TPU v6e 4x4
 sed -i 's/train_batch_size=train_batch_size/train_batch_size=32/g' /root/axlearn/experiments/text/gpt/fuji.py
 
