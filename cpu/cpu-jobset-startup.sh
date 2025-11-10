850bb784a014911fe9b63a435edf7d0d6e2b12fd
Camilo Quinones <camilo.quinones@cienet.com>
2025-11-10 09:27:05 +0000

    Add: Add dynamic POST_COMMAND for CPU nightly workflow. New build cpu-nightly-image

--- cpu/cpu-jobset-startup.sh
+++ cpu/cpu-jobset-startup.sh
@@ -11,6 +11,17 @@ export UV_FIND_LINKS="https://storage.googleapis.com/jax-releases/libtpu_release
 echo "UV links: ${UV_FIND_LINKS}"
 cd /root && uv pip install .[core,dev,gcp,open_api,audio] pytest pytest-instafail pytest-xdist pytest-csv pytest-timeout
 
+# Remove CUDA-enabled TensorFlow and install CPU-only variant
+TF_VER=$(pip freeze | grep -w tensorflow= | awk -F '==' {'print $2'}) && \
+    pip uninstall -y -qq tensorflow && \
+    uv pip install --no-deps tensorflow-cpu==$TF_VER && \
+    uv cache clean
+
+# Run any post-setup command if defined and not set to INSERT_POST_SETUP_CMD
+if [ "$POST_SETUP_CMD" != "INSERT_POST_SETUP_CMD" ]; then
+    eval "$POST_SETUP_CMD"
+fi
+
 # Run the unit tests
 if [ $? -eq 0 ]; then
     bash /var/arc/cpu-unit-tests.sh 2>&1 | tee -a /home/runner/_work/csv_results/full_output.log
