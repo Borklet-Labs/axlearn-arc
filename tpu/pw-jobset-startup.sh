be3375339d53025c7f709ee2fc410b74a1d71b66
Quinn McGarry <quinn.mcgarry@cienet.com>
2026-02-24 17:58:59 +0000

    Test added for Pathways with v6e TPU

---
+++ tpu/pw-jobset-startup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Check for custom origin
+if [ -z "$CUSTOM_GIT_ORIGIN" ]; then
+    GIT_ORIGIN="https://github.com/Borklet-Labs/axlearn"
+else
+    GIT_ORIGIN="$CUSTOM_GIT_ORIGIN"
+fi
+
+# Check for branch name
+if [ -z "$CUSTOM_GIT_BRANCH" ]; then
+    GIT_BRANCH="main"
+else
+    GIT_BRANCH="$CUSTOM_GIT_BRANCH"
+fi
+
+echo "About to pull branch $GIT_BRANCH from origin $GIT_ORIGIN"
+
+# Grab the latest AXLearn from upstream
+git init /root && cd /root 
+git remote add origin $GIT_ORIGIN
+git -c protocol.version=2 fetch --no-tags --prune --no-recurse-submodules --depth=1 origin
+git checkout $GIT_BRANCH
+
+# Show the commit information
+git log -1 --stat --pretty=format:"%H" --no-patch
+
+if [ "$ENABLE_JAX_DEV" == "true" ]; then
+    echo "Enabling prerelease Jax and specifying extra idnex"
+    export UV_PRELEASE=allow
+    export UV_INDEX=https://us-python.pkg.dev/ml-oss-artifacts-published/jax/simple/
+fi
+
+uv pip install .[core,tpu]
+
+# Run any post-setup command if defined and not set to INSERT_POST_SETUP_CMD
+if [ "$POST_SETUP_CMD" != "INSERT_POST_SETUP_CMD" ]; then
+    eval "$POST_SETUP_CMD"
+fi
+
+# Modify the batch size to account for TPU v6e 4x4
+sed -i 's/train_batch_size=train_batch_size/train_batch_size=32/g' /root/axlearn/experiments/text/gpt/fuji.py
+
+# Start the training loop
+python3 -m axlearn.common.launch_trainer_main \
+    --module=text.gpt.c4_trainer --config=fuji-7B-v2-flash \
+    --trainer_dir=${GCS_PREFIX}/runs/${GIT_BRANCH}/${GH_RUN_ID} \
+    --data_dir=gs://axlearn-public/tensorflow_datasets \
+    --jax_backend=proxy \
+    --mesh_selector=tpu-v6e-16 \
+    --trace_at_steps=5 \
+    --trainer_log_every_n_steps=1
