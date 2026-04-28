fd14cc44fc207d12867b1849c8393aee7e3915f5
Judy Wu <judyzhaoxinwu@google.com>
2026-04-28 21:34:47 +0000

    Test latest AXLearn pulled changes and fixes with jax nightly

--- tpu/tpu-jobset-startup.sh
+++ tpu/tpu-jobset-startup.sh
@@ -17,7 +17,7 @@ fi
 echo "About to pull branch $GIT_BRANCH from origin $GIT_ORIGIN"
 
 # Grab the latest AXLearn from upstream
-git init /root && cd /root
+git init /root && cd /root 
 git remote add origin $GIT_ORIGIN
 git -c protocol.version=2 fetch --no-tags --prune --no-recurse-submodules --depth=1 origin
 git checkout $GIT_BRANCH
@@ -42,30 +42,16 @@ JAX_VER=$(python3 -c 'import jax; print(jax.version.__version__)')
 echo "JAX_VERSION_OUTPUT:${JAX_VER}"
 
 gsutil -h "x-goog-meta-jax-version:${JAX_VER}" -m cp /dev/null ${GCS_PREFIX}/metadata/jax_version_tag_${GH_RUN_ID}
+
 # Modify the batch size to account for TPU v6e 4x4
 sed -i 's/train_batch_size=train_batch_size/train_batch_size=32/g' /root/axlearn/experiments/text/gpt/fuji.py
 
-# Set max_step to 1 for the first run
-sed -i '/max_step = TOTAL_TOKENS\[version\]\[model_size\] \/\/ tokens_per_batch/a \    max_step = 1' /root/axlearn/experiments/text/gpt/fuji.py
-
-sed -i 's|learner_kwargs=dict(peak_lr=3e-4, weight_decay=0.1),|learner_kwargs=dict(peak_lr=3e-4, weight_decay=0.1, lr_warmup_steps=0),|g' /root/axlearn/experiments/text/gpt/fuji.py
-
+# Start the training loop
 python3 -m axlearn.common.launch_trainer_main \
     --module=text.gpt.c4_trainer --config=fuji-7B-v2-flash \
     --trainer_dir=${GCS_PREFIX}/runs/${GIT_BRANCH}/${GH_RUN_ID} \
     --data_dir=gs://axlearn-public/tensorflow_datasets \
     --jax_backend=tpu \
     --mesh_selector=tpu-v6e-16 \
-    --trainer_log_every_n_steps=1
-
-# --- Run 2: Resume and Stop at Step 2 ---
-# Now change any instance of max_step=1, to max_step=2,
-sed -i 's/max_step = 1/max_step = 2/g' /root/axlearn/experiments/text/gpt/fuji.py
-
-python3 -m axlearn.common.launch_trainer_main \
-    --module=text.gpt.c4_trainer --config=fuji-7B-v2-flash \
-    --trainer_dir=${GCS_PREFIX}/runs/${GIT_BRANCH}/${GH_RUN_ID} \
-    --data_dir=gs://axlearn-public/tensorflow_datasets \
-    --jax_backend=tpu \
-    --mesh_selector=tpu-v6e-16 \
+    --trace_at_steps=5 \
     --trainer_log_every_n_steps=1
